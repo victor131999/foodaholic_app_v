1364b2df3476aae1f1b27e7ccb9d335b42ff17a4
victor131999 <tovir.gu.hu@hotmail.com>
2021-01-21 19:31:16 -0500

    Llamada al backend para el carrucel

--- foodaholic_app_full/foodaholic_app_v/lib/widgets/various_menu_widget.dart
+++ foodaholic_app_full/foodaholic_app_v/lib/widgets/various_menu_widget.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+/*import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_options.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:foodaholic_app_v/models/menu_model.dart';
@@ -131,4 +131,178 @@ class CurrentSpecialWidget extends StatelessWidget {
     );
   }
 
+}*/
+import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_options.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:foodaholic_app_v/models/menu_model.dart';
+import 'package:foodaholic_app_v/providers/reservation_Service.dart';
+
+class CurrentSpecialWidget extends StatefulWidget {
+  CurrentSpecialWidget({Key key}) : super(key: key);
+
+  @override
+  _CurrentWidgetState createState() => _CurrentWidgetState();
+}
+
+class _CurrentWidgetState extends State<CurrentSpecialWidget> {
+  final formKey = GlobalKey<FormState>();
+  
+  Menus _list;
+  MenusService _service;
+
+  @override
+  void initState() {
+    _service = new MenusService();
+    super.initState();
+    _loadMenus();
+  }
+
+@override
+  Widget build(BuildContext context) {
+
+
+    int _current = 0;
+  
+    final List<String> imgList = [
+
+      'https://www.ecured.cu/images/4/40/Hamburguesa1.jpg',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Shawarma_%283157609887%29.jpg/1200px-Shawarma_%283157609887%29.jpg',
+      'https://www.mrpollo.com.ec/images/articulos/2017/06/historia_2.jpg',
+      'https://img.vixdata.io/pd/jpg-large/es/sites/default/files/imj/elgranchef/O/Origen-de-las-papas-fitas-1.jpg',
+      'https://saposyprincesas.elmundo.es/wp-content/uploads/2017/01/comida-india-langostinos-tandoori.jpg',
+      'https://live.mrf.io/statics/i/ps/www.cocinacaserayfacil.net/wp-content/uploads/2019/11/Comida-china.jpg?width=1200&enable=upscale'
+    ];
+
+  final List<Widget> imageSliders = imgList.map((item) => Container(
+  child: Container(
+    margin: EdgeInsets.all(5.0),
+    child: ClipRRect(
+      borderRadius: BorderRadius.all(Radius.circular(5.0)),
+      child: Stack(
+        children: <Widget>[
+          Image.network(item, fit: BoxFit.cover, width: 1000.0),
+          Positioned(
+            bottom: 0.0,
+            left: 0.0,
+            right: 0.0,
+            child: Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    Color.fromARGB(200, 0, 0, 0),
+                    Color.fromARGB(0, 0, 0, 0)
+                  ],
+                  begin: Alignment.bottomCenter,
+                  end: Alignment.topCenter,
+                ),
+              ),
+              padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+              child: Text(
+                'No. ${imgList.indexOf(item)} nombre',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+        ],
+      )
+    ),
+    
+  ),
+  
+)).toList();
+
+    double _heigth=MediaQuery.of(context).size.height;
+    print("Altura: $_heigth");
+
+    return Scaffold(
+      appBar: AppBar(title: Text('Especiales de Food Holic')),
+      body: Column(
+        
+        children: [
+          CarouselSlider(
+            items: imageSliders,
+            options: CarouselOptions(
+              autoPlay: true,
+              enlargeCenterPage: true,
+              aspectRatio: 2.0,
+              onPageChanged: (index, reason) {
+                 {
+                  _current = index;
+                }
+              }
+            ),
+          ),
+          Row(
+            
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: imgList.map((url) {
+              int index = imgList.indexOf(url);
+              return Container(
+                width: 8.0,
+                height: 8.0,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 2.0),
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: _current == index
+                    ? Color.fromRGBO(0, 0, 0, 0.9)
+                    : Color.fromRGBO(0, 0, 0, 0.4),
+                ),
+                
+              );
+              
+            }).toList(),
+            
+          ),
+          
+
+    
+        ]
+        
+      )
+    );
+    
+    
+  }
+  
+
+  Widget Name(BuildContext context) {
+    return  ListView(
+            children: _list.items.map((e) {
+            return _getNameItem(e);
+          }).toList()
+          );
+  }
+
+@override
+  Widget ImageLink(BuildContext context) {
+    return  ListView(
+            children: _list.items.map((e) {
+            return _getImageItem(e);
+          }).toList()
+          );
+  }
+  _loadMenus() {
+    _service.getMenus(1, 6).then((value) {
+      setState(() {
+        _list = value;
+      });
+    });
+  }
+
+  Widget _getNameItem(Menu menu) {
+    return 
+            Text(menu.name);
+  }
+
+  Widget _getImageItem(Menu menu) {
+    return 
+            Text(menu.image);
+  }
+
+  
 }
