d8424f5a60d5b4a17b52f2baeb0a4a771962db8a
victor131999 <tovir.gu.hu@hotmail.com>
2021-02-11 00:31:46 -0500

    Current location

--- foodaholic_app_full/foodaholic_app_v/lib/widgets/current_location_widget.dart
+++ foodaholic_app_full/foodaholic_app_v/lib/widgets/current_location_widget.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class CurrentLocationWidget extends StatefulWidget {
@@ -17,8 +18,18 @@ class _CurrentLocationWidgetState extends State<CurrentLocationWidget> {
     target: LatLng(-0.9577570609759557, -78.69691968848863),
     zoom: 18,
   );
-
-
+  CameraPosition _kCurrentPosition;
+  @override
+  void initState(){
+    super.initState();
+    _determinePosition().then((value) { 
+        _kCurrentPosition=CameraPosition(
+          target: LatLng(value.latitude, value.longitude),
+          zoom: 18,
+        );
+        _goToCurrentPosition() ;
+    });
+  }
   @override
   Widget build(BuildContext context) {
     double _heigth = MediaQuery.of(context).size.height;
@@ -68,4 +79,38 @@ class _CurrentLocationWidgetState extends State<CurrentLocationWidget> {
       ),
     );
   }
+
+
+Future<Position> _determinePosition() async {
+  bool serviceEnabled;
+  LocationPermission permission;
+
+  serviceEnabled = await Geolocator.isLocationServiceEnabled();
+  if (!serviceEnabled) {
+    return Future.error('Location services are disabled.');
+  }
+
+  permission = await Geolocator.checkPermission();
+  if (permission == LocationPermission.deniedForever) {
+    return Future.error(
+        'Location permissions are permantly denied, we cannot request permissions.');
+  }
+
+  if (permission == LocationPermission.denied) {
+    permission = await Geolocator.requestPermission();
+    if (permission != LocationPermission.whileInUse &&
+        permission != LocationPermission.always) {
+      return Future.error(
+          'Location permissions are denied (actual value: $permission).');
+    }
+  }
+
+  return await Geolocator.getCurrentPosition();
+}
+
+Future<void> _goToCurrentPosition() async {
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(_kCurrentPosition));
+  }
+
 }
